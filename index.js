a719f7c7a3def7785532a0e9f558be0f524b63d5
Keith Powell <keith@tapcart.co>
2021-09-08 11:15:10 -0400

    Added animation to button clicked

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+
+// This section of code captures the buttons in the DOM and adds an Event Listener to each button.  
+// querySelectorAll creates an array and the FOR loop loops thru the array to place the Event listener
+
 const buttons = document.querySelectorAll(".drum")
+
+for (let i = 0; i < buttons.length; i++) {
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        let buttonInnerHTML = this.innerHTML
+        
+        drumSound(buttonInnerHTML)
+
+        makeAnimation(buttonInnerHTML)
+    })
+}
+
+// This Event Handler listens for the corresponding key press
+document.addEventListener("keydown", function(event) {
+    
+    drumSound(event.key)
+
+    makeAnimation(event.key)
+})
+
+// Functions that the Event Handlers are using
 
 function drumSound (key) {
 
@@ -40,73 +66,16 @@ function drumSound (key) {
             break;
 
         default:
-            console.log(buttonInnerHTML);
+            console.log(key);
             break;
     }
 }
 
-for (let i = 0; i < buttons.length; i++) {
+function makeAnimation(currentKey) {
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    const activeButton = document.querySelector(`.${currentKey}`)
+    
+    activeButton.classList.toggle("pressed")
 
-        let buttonInnerHTML = this.innerHTML
-
-        drumSound(buttonInnerHTML)
-    })
+    
 }
-
-document.addEventListener("keydown", function(event) {
-
-    drumSound(event.key)
-})
-
-
-// document.addEventListener("keydown", function (e) {
-
-//     let keyPressed = e.key
-
-//     switch (keyPressed) {
-
-//         case "w":
-//             let crash = new Audio("sounds/crash.mp3")
-//             crash.play();
-//             break;
-
-//         case "a":
-//             let kick = new Audio("sounds/kick-bass.mp3")
-//             kick.play();
-//             break;
-
-//         case "s":
-//             let snare = new Audio("sounds/snare.mp3")
-//             snare.play();
-//             break;
-
-//         case "d":
-//             let tom1 = new Audio("sounds/tom-1.mp3")
-//             tom1.play();
-//             break;
-
-//         case "j":
-//             let tom2 = new Audio("sounds/tom-2.mp3")
-//             tom2.play();
-//             break;
-
-//         case "k":
-//             let tom3 = new Audio("sounds/tom-3.mp3")
-//             tom3.play();
-//             break;
-
-//         case "l":
-//             let tom4 = new Audio("sounds/tom-4.mp3")
-//             tom4.play();
-//             break;
-
-//         default:
-//             console.log(keyPressed);
-//             break;
-//     }
-// })
-
-// let audio = new Audio("sounds/tom-1.mp3")
-// audio.play();
